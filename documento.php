71bb0b8f4c0ccd326dd2e3c615446fe0a8db49db
Gianmarco Loyola <gian711301@gmail.com>
2019-05-29 10:41:25 -0500

    Archivo añadido

---
+++ documento.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo/estilos.css">
+</head>
+<body>
+    <h1 class="center">Bienvenidos al portal "Animales Sin Hogar"</h1>
+    <h2>¿Vio a un animal perdido? Registrelo aqui!</h2>
+    <table border="1">
+        <tr>
+            <th>Raza</th>
+            <th>¿Donde lo vio?</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Registrar</a></td>
+        </tr>
+    </table>
+</body>
